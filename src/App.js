40910bbff9931b0a5731aebc69ca693836089d21
govindv416 <113519241+govindv416@users.noreply.github.com>
2023-06-30 00:27:40 +0530

    Update App.js
    App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Routes,Route
+} from "react-router-dom";
+import React from 'react';
+import Home from './Components/Home';
+import NetflixShow from './Components/NetflixShow';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+    
+      <div className='App'>
+      <Routes>
+        <Route exact path='/' Component={Home}/>
+        <Route exact path='netflix-show' Component={NetflixShow}/>
+       </Routes>
+      </div>
+      </Router>
   );
 }
 
